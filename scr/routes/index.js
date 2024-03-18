73f1a607a59ab1fae355dba32762fc304f96805b
Stas <stas.krivosheev.0054@gmail.com>
2024-03-18 20:57:43 +0300

    first commit

---
+++ scr/routes/index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const usersRoutes = require('./usersRoutes')
+
+const router = express.Router()
+
+router.use('/users',usersRoutes)
+
+module.exports = router;
---
+++ scr/routes/usersRoutes.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router =express.Router()
+
+
+app.get('/api/echo',(req,res) => { //&&&
+    res.send('Привет,Redev!')
+})
